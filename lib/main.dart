018bda09c61bf7898fd6acc66c2bc018493c1def
ELIZIEL_CAMPEROS_GARCIA <a.22308051281159@cbtis128.edu.mx>
2025-02-19 20:31:34 +0000

    appbar

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       // Application name
-      title: 'Flutter Hello World',
+      title: 'Eliziel Flutter',
       // Application theme data, you can set the colors for the application as
       // you want
       theme: ThemeData(
@@ -17,25 +18,40 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Eliziel Camperos'),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});  
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.abc_sharp,
+            ),
+            onPressed: () {},
+          )
+        ],
+        backgroundColor: Colors.amber,
+        leading: Icon(Icons.alarm),
+        centerTitle: true,
         // The title text which will be shown on the action bar
         title: Text(title),
       ),
       body: Center(
         child: Text(
-          'Hello, World!',
+          'hola Camperos con appbar!',
         ),
       ),
     );
